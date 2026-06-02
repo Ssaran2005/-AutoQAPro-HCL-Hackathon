7440737645a865eb434c39b79b3b6fbe4315327a
Ssaran2005 <saransakthivel2005@gmail.com>
2026-06-02 21:08:47 +0530

    Added BasePage with explicit wait utility

---
+++ src/test/java/base/BasePage.java
@@ -0,0 +1,31 @@
+package base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+
+        this.driver = driver;
+
+        wait = new WebDriverWait(
+                driver,
+                Duration.ofSeconds(10));
+    }
+
+    public WebElement waitForElement(By locator) {
+
+        return wait.until(
+                ExpectedConditions
+                        .visibilityOfElementLocated(locator));
+    }
+}
